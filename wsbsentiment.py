140eb217d5b2df59878e906f5577a0a7635c95b9
Sejin Kim <kim3@kenyon.edu>
2022-04-12 10:20:22 -0400

    Add flair support

--- wsbsentiment.py
+++ wsbsentiment.py
@@ -22,22 +22,55 @@ headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
 while True:
         # while the token is valid (~2 hours) we just add headers=headers to our requests
         response = requests.get("https://oauth.reddit.com/r/wallstreetbets/random", headers = headers)
+        #print(response.json()[0]['data']['children'][0]['data'])
 
         title = response.json()[0]['data']['children'][0]['data']['title']
         title = title.replace('",', '')
         title = re.sub(r"[^\d\w\s$'\.]", '', title)
-        print('Title: ' + title)
+        
+
+        flair = response.json()[0]['data']['children'][0]['data']['link_flair_richtext'][0]['t']
+        flair = re.sub(r"[^\d\w\s]", '', flair).lower()[:4]
         if title in seen:
                 print("Post already seen. Passing...")
                 time.sleep(1.2)
+        if flair in ['meme']:
+                print("Meme. Passing...")
+                if title not in seen:
+                        seen.append(title)
+                        file = open("./seen", 'a')
+                        file.write('\n' + title)
+                        file.close()
+                time.sleep(1.2)
+        if flair in ['gain']:
+                print('Autoranking based on flair')
+                file = open("./wsbsentiment.csv", 'a')
+                file.write('"' + title + '","' + text + '","positive"\n')
+                file.close()
+                seen.append(title)
+                file = open("./seen", 'a')
+                file.write('\n' + title)
+                file.close()
+                time.sleep(1.2)
+        if flair in ['loss']:
+                print('Autoranking based on flair')
+                file = open("./wsbsentiment.csv", 'a')
+                file.write('"' + title + '","' + text + '","negative"\n')
+                file.close()
+                seen.append(title)
+                file = open("./seen", 'a')
+                file.write('\n' + title)
+                file.close()
+                time.sleep(1.2)
         else:
+                print('Title: ' + title)
                 text = response.json()[0]['data']['children'][0]['data']['selftext']
                 text = re.sub(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", '', text)
                 text = text.replace('",', '')
                 text = text.replace('\n', ' ')
                 text = re.sub(r"[^\d\w\s$'\.]", '', text)
-                print('Self Text: ' + text)
-
+                print('Text: ' + text)
+                print('Flair: ' + flair)
                 rank = str(input('Rank the post:\n1-3 or `n`\tNegative\n4-6 or `k`\tNeutral\n7-9 or `p`\tPositive\n0 or space\tSkip\nx\t\tExit\n>>> '))
                 if rank in ['1', '2', '3', 'n']:
                         file = open("./wsbsentiment.csv", 'a')
@@ -70,4 +103,4 @@ while True:
                         file.close()
                 elif rank in ['x']:
                         sys.exit()
-                time.sleep(2)
+                time.sleep(1)
